e69a61dfc6a1548cd1bda1e97553e59bca6a719e
Neeraj Balachandar <128891743+neerajbalachandar@users.noreply.github.com>
2025-06-11 18:37:44 +0530

    Update maneuver_definition.jl

--- src/dragonfly/maneuver_definition.jl
+++ src/dragonfly/maneuver_definition.jl
@@ -4,6 +4,26 @@ __contact__ = "neerajbalachandar@gmail.com"
 
 #--------------WILL BE UPDATED BASED ON SOLVING THE VEHICLE KINEMATIC EQUATIONS TO COUPLE THE CONTROL INPUT WITH KINEMATICS, EITHER BY EXPERIMENTAL VALIDATION FOR HOVERING CONDITION AND FORWARD FLIGHT OR THROUGH SIMULATION-----
 #-----------------------------------------------------------FIXED VEHICLE WITH SYNCHRONISED PHASE FLAPPING OF SAME AMPLITUDE AND FREQUENCY FOR EACH WING----------------------
+"""
+    KinematicManeuver{N, M}(angle, RPM, Vvehicle, anglevehicle)
+
+A vehicle maneuver that prescribes the kinematics of the vehicle through the
+functions `Vvehicle` and `anglevehicle`. Control inputs to each tilting and 
+systems are given by the function `angle`.
+
+# ARGUMENTS
+* `angle::NTuple{N, Function}` where `angle[i](t)` returns the angles
+        `[Ax, Ay, Az]` (in degrees) of the i-th tilting system at time `t` (t is
+        nondimensionalized by the total time of the maneuver, from 0 to 1,
+        beginning to end).
+* `Vvehicle::Function` where `Vvehicle(t)` returns the normalized vehicle
+        velocity `[Vx, Vy, Vz]` at the normalized time `t`. The velocity is
+        normalized by a reference velocity (typically, cruise velocity).
+* `anglevehicle::Function` where `anglevehicle(t)` returns the angles
+        `[Ax, Ay, Az]` (in degrees) of the vehicle relative to the global
+        coordinate system at the normalized time `t`.
+"""
+
 
 function generate_dragonfly_maneuver(; disp_plot = true, add_wings = true, vehicle_velocity::Real=0.0, angle_of_attack::Real=0.0)
     
