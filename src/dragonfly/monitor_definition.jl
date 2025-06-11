7c5b5675d7d7bc29f5ccbb3bc780e1666745280c
Neeraj Balachandar <128891743+neerajbalachandar@users.noreply.github.com>
2025-06-11 19:26:29 +0530

    Update monitor_definition.jl

--- src/dragonfly/monitor_definition.jl
+++ src/dragonfly/monitor_definition.jl
@@ -1,1 +1,5 @@
+#--------------------------------------------------------------------------------NEERAJ BALACHANDAR-----------------------------------------------------------------------------------------------------
+__authors__ = "NEERAJ BALACHANDAR"
+__contact__ = "neerajbalachandar@gmail.com"
 
+function generate_dragonfly_monitor()
